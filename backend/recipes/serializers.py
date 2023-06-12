76530afd761d89656b3834aa358bc4f6eb2e7795
tim714 <ildarnigmetzjanov@gmail.com>
2023-06-12 22:54:44 +0300

    Правки

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -88,7 +88,7 @@ class AddRecipeSerializer(serializers.ModelSerializer):
                                               many=True)
     image = Base64ImageField()
     author = CustomUserSerializer(read_only=True)
-    cooking_time = serializers.IntegerField()
+    # cooking_time = serializers.IntegerField()
 
     class Meta:
         model = Recipe
