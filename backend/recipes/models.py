e9eace88c57c4a1821e8852001f562c57c7bc1c4
tim714 <ildarnigmetzjanov@gmail.com>
2023-06-15 23:53:03 +0300

    Правка

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -179,7 +179,7 @@ class ShoppingList(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='shopping_lists',
+        # related_name='shopping_lists',
         verbose_name='Рецепт'
     )
 
